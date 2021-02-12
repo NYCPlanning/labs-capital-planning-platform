f5216ec50aea25be1ec93d11cdbccf13cdfa9b44
godfreyyeung <godfreyyeung@gmail.com>
2021-02-11 22:41:51 -0800

    Load facility categories using Carto SQL API

--- src/LayersList.js
+++ src/LayersList.js
@@ -4,22 +4,22 @@ import CategoryWidget from './widgets/category';
 export default class LayersList extends React.Component {
   state = {
     categories: [],
-    selection: [],
-    filter: null,
   };
 
-  facilitiesCategories =  [
-    { name: 'LIBRARIES AND CULTURAL PROGRAMS', value: 1000 },
-    { name: 'PARKS, GARDENS, AND HISTORICAL SITES', value: 900 },
-    { name: 'HEALTH AND HUMAN SERVICES', value: 800 },
-  ];
+  async componentDidMount() {
+    const facilityCategoriesPromise = await fetch('https://planninglabs.carto.com/api/v2/sql?q=SELECT facdomain, COUNT(*) AS count FROM facdb_v2019_12 GROUP BY facdomain');
+    const { rows: rawFacilityCategories } = await facilityCategoriesPromise.json();
 
+    this.setState({
+      categories: rawFacilityCategories.map(row => { return { name: row.facdomain, value: row.count} }),
+    });
+  }
 
   render() {
     return (
       <CategoryWidget
         heading="Facilities Categories"
-        categories={this.facilitiesCategories}
+        categories={this.state.categories}
         onSelectedChanged={this.props.onFilterSelection}
       />
     );
