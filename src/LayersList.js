405a509cdcc44b482f887b45935b68440682ee58
godfreyyeung <godfreyyeung@gmail.com>
2021-02-11 22:53:03 -0800

    Move fetched facility cat data up to App component

--- src/LayersList.js
+++ src/LayersList.js
@@ -2,24 +2,11 @@ import React from 'react';
 import CategoryWidget from './widgets/category';
 
 export default class LayersList extends React.Component {
-  state = {
-    categories: [],
-  };
-
-  async componentDidMount() {
-    const facilityCategoriesPromise = await fetch('https://planninglabs.carto.com/api/v2/sql?q=SELECT facdomain, COUNT(*) AS count FROM facdb_v2019_12 GROUP BY facdomain');
-    const { rows: rawFacilityCategories } = await facilityCategoriesPromise.json();
-
-    this.setState({
-      categories: rawFacilityCategories.map(row => { return { name: row.facdomain, value: row.count} }),
-    });
-  }
-
   render() {
     return (
       <CategoryWidget
         heading="Facilities Categories"
-        categories={this.state.categories}
+        categories={this.props.categoryData.facilityDomain}
         onSelectedChanged={this.props.onFilterSelection}
       />
     );
