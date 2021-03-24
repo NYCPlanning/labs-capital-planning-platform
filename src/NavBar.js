0bb12ec87c110b7a907fccfb60d5348019eec5e6
godfreyyeung <godfreyyeung@gmail.com>
2021-03-24 13:45:24 -0700

    Style header

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,6 +7,9 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: "#fefefe",
+    color: "#d96b27",
+    fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",sans-serif',
   },
 }));
 
