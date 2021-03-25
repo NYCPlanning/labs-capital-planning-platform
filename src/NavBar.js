8fccea71771f7a1c3fa1d9fb8e3d839343136bec
godfreyyeung <godfreyyeung@gmail.com>
2021-03-24 22:43:23 -0700

    Add  DCP Logo to NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import DcpLogo from './assets/dcp_logo_772.png';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,6 +12,11 @@ const useStyles = makeStyles((theme) => ({
     color: "#d96b27",
     fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",sans-serif',
   },
+  dcpLogo: {
+    height: '2.25rem',
+    paddingRight: 10,
+    verticalAlign: 'bottom',
+  }
 }));
 
 export default function NavBar() {
@@ -20,6 +26,11 @@ export default function NavBar() {
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
         <Typography variant="h6" noWrap>
+          <img
+            src={DcpLogo}
+            className={classes.dcpLogo}
+            alt="Department of City Planning Logo"
+          />
           Capital Planning Platform
         </Typography>
       </Toolbar>
