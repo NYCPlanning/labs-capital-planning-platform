1896d7f22a32f6de3d895be0d289069ef551fa0b
godfreyyeung <godfreyyeung@gmail.com>
2021-03-31 13:20:19 -0700

    Use worker-loader to patch mapbox-gl bug

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
+import mapboxgl from "mapbox-gl";
 import {StaticMap} from 'react-map-gl';
 import {CartoSQLLayer, setDefaultCredentials, colorCategories} from '@deck.gl/carto';
 import {
   useHistory,
 } from "react-router-dom";
+
+// https://github.com/mapbox/mapbox-gl-js/issues/10173#issuecomment-750489778
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const INITIAL_VIEW_STATE = {
   longitude: -73.986607,
